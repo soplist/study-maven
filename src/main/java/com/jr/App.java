be2ad4e409ccdecb6a40207b54989581e5fd455f
soplist <sopcase@yahoo.com>
2016-04-29 12:03:09 +0800

    simple maven test

---
+++ src/main/java/com/jr/App.java
@@ -0,0 +1,25 @@
+package com.jr;
+
+import java.util.UUID;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        App obj = new App();
+        System.out.println("Unique ID:"+obj.generateUniqueKey());
+    }
+    
+    public String generateUniqueKey(){
+    	
+    	String id = UUID.randomUUID().toString();
+    	return id;
+    	
+    }
+}
